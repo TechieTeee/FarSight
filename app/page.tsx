fa7f7d4df00e31ea23e1c3d09fe4ac1f1ee10759
TechieTeee <techieteee@protonmail.com>
2024-03-24 13:50:58 +0000

    Updated app for styles import

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import '/styles/global.css';
 
 const frameMetadata = getFrameMetadata({
   buttons: [
